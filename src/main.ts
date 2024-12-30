805c85358b0bc4bd8b0c4397cf07777b14864e1b
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2024-12-30 03:40:41 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+import { envs } from './products/config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen();
+  logger.log(`Products microservices running on port ${envs.port}`);
+}
+bootstrap();
