6107e6e94851da78c6271a9caf18ea4b6ac6ccd9
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2024-12-30 07:23:29 -0500

    Section-05
    Manejo de errores con el uso del client gateway

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
 
 @Injectable()
@@ -44,7 +46,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       where: {id, available:true},
     });
 
-    if(!product) throw new NotFoundException(`Product with id #${id} not found`);
+    if(!product) throw new RpcException({
+      message: `Product with id #${id} not found`,
+      status:HttpStatus.BAD_REQUEST
+    });
 
     return product;
   }
